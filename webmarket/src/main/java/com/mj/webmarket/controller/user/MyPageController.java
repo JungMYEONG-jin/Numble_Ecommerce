6392808b7ebc8b1ecd388fe550fd07d833a3962e
a60156077 <dwrhksfl77@naver.com>
2022-10-04 14:54:43 +0900

    내 페이지 특정 상품 정보 보기 구현완료..

--- webmarket/src/main/java/com/mj/webmarket/controller/user/MyPageController.java
+++ webmarket/src/main/java/com/mj/webmarket/controller/user/MyPageController.java
@@ -1,7 +1,11 @@
 package com.mj.webmarket.controller.user;
 
+import com.mj.webmarket.entity.dto.product.ProductDetailResponse;
 import com.mj.webmarket.entity.dto.user.UserResponseDto;
+import com.mj.webmarket.entity.product.Product;
+import com.mj.webmarket.entity.product.ProductStatus;
 import com.mj.webmarket.entity.user.User;
+import com.mj.webmarket.exception.ProductNotFoundException;
 import com.mj.webmarket.service.product.ProductServiceImpl;
 import com.mj.webmarket.service.user.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @Slf4j
@@ -30,9 +35,23 @@ public class MyPageController {
         return "mypage/myPage";
     }
 
-    @GetMapping("/mypage/products")
-    public String showMyProducts(@AuthenticationPrincipal UserDetails userDetails, Model model){
+    @GetMapping("/mypage/products/{productId}")
+    public String showMySpecificProduct(@PathVariable Long productId, @AuthenticationPrincipal UserDetails userDetails, Model model){
+        String email = userDetails.getUsername();
+        Product product = productService.findOneById(productId);
+        if (!email.equals(product.getUser().getEmail()))
+            throw new ProductNotFoundException("해당 판매자 정보가 일치하는 상품이 없습니다..");
 
+        // user 정보 찾기
+        User user = userService.findUser(email);
+        UserResponseDto userInfo = userService.toUserResponseDto(user);
+
+        ProductDetailResponse productDetailResponse = productService.toProductResponseDto(product);
+        ProductStatus productStatus = productDetailResponse.getProductStatus();
+
+        model.addAttribute("userInfo", userInfo);
+        model.addAttribute("product", productDetailResponse);
+        model.addAttribute("productStatus", productStatus);
 
         return "mypage/myProductDetails";
     }
